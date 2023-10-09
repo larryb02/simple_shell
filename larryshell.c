4795e242658746e6f3e651688ba53a5b79eb8dc0
Larry Banks <larrykbanks21@gmail.com>
2023-10-09 13:20:03 -0400

    test commit

--- larryshell.c
+++ larryshell.c
@@ -4,13 +4,13 @@
 
 #define DEF_BUFFERSIZE 1024
 #define TOK_BUFFER 64
-#define DELIM '-'
+#define DELIM "-"
 
 
 
 void programLoop(void);
 char *readLine(void);
-char *parseArgs(const char *input);
+char **parseArgs(char *input);
 
 //setup program loop
 
@@ -25,12 +25,15 @@ int main(int argc, char **argv){
 
 void programLoop(void){
 char *input;
-char *args;
+char **args;
 	
     do{
 	printf("%s", "-> ");
 	input = readLine();
-	//args = parseArgs(input);
+	args = parseArgs(input);
+
+	free(input);
+	free(args);
 	}while(1);
 	
 
@@ -76,18 +79,30 @@ int c;
 
 }
 
-char **parseArgs(const char *input){
-
+char **parseArgs(char *input){
+int bufferSize = TOK_BUFFER;
 char *token;
 char **tokens = malloc(sizeof(char) * TOK_BUFFER);
 int position = 0;
 
+if(!tokens){
+fprintf(stderr, "Allocation error.");
+exit(EXIT_FAILURE);
+}
 
 token = strtok(input, DELIM);
-	while(token != null){
-		tokens[position] = args; 		
+	while(token != NULL){
+		tokens[position] = token; 		
 		position++;	
-
+		if(position >= bufferSize){
+			bufferSize += TOK_BUFFER;
+			tokens = realloc(tokens, bufferSize);
+			if(!tokens){
+				fprintf(stderr, "Allocation error.");
+				exit(EXIT_FAILURE);
+				}
+		
+		}
 	}
-return args;
+return tokens;
 }
