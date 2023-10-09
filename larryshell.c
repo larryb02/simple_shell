000871fa88ae1bfd7c1490a88b13a332a6ca1e1c
Larry Banks <larrykbanks21@gmail.com>
2023-10-08 22:54:11 -0400

    finished readLine()

--- larryshell.c
+++ larryshell.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define DEF_BUFFERSIZE 1024
+#define DELIM '-'
 
 
 
-void programLoop();
+void programLoop(void);
+char *readLine(void);
+char *parseArgs(const char *input);
 
 //setup program loop
 
@@ -16,14 +22,70 @@ int main(int argc, char **argv){
 }
 
 
-void programLoop(){
+void programLoop(void){
     char *input;
     char *args;
+	
+    	do{
+	printf("%s", "-> ");
+	input = readLine();
+	//args = parseArgs(input);
+	}while(1);
+	
 
-	do{
-		printf("%s","->");
+}
+
+char *readLine(void){
+
+	int bufferSize = DEF_BUFFERSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufferSize);
+	int c;
+
+	if(!buffer){
+	fprintf(stderr, "allocation error.\n");
+	exit(EXIT_FAILURE);
+	}
+
+	while(1){
+	
+	 c = getchar();
+
+	 if(c == EOF || c == '\0'){
+		buffer[position] = '\0'; 
+		return buffer;
+	 }
+	 else{
+	 	buffer[position] = c;
+	 }
+	 position++;
+
+	if(position >= bufferSize){
+		bufferSize += DEF_BUFFERSIZE;
+		buffer = realloc(buffer, bufferSize);
+
+		if(!buffer){
+			fprintf(stderr, "allocation error.\n");
+			exit(EXIT_FAILURE);
+			}
+		}
+	
+	}
+	
+
+}
+
+char *parseArgs(const char *input){
+
+char *args;
+int c;
+int position = 0;
+
+	while(input[position] != '\0'){
 		
 
-	}while(1);
+	position++;	
 
+	}
+return args;
 }
